848e5bbf613459130e0bf8898e419ddc844b6c2a
vaithorat <vaibhavthorat141997@gmail.com>
2022-07-31 17:20:23 +0530

    1.3 Case insensitive user input

--- js.js
+++ js.js
@@ -1,28 +1,34 @@
 let userChoice = prompt("Choose Rock, Paper or Scissor");
-console.log(userChoice);
+console.log(userChoice.toLocaleLowerCase("en-US"));
+// getComputerChoice();
+let rock = "Rock"
+let str1 = rock.toLocaleLowerCase("en-US");
+let paper = "Paper"
+let str2 = paper.toLocaleLowerCase("en-US");
+let scissor = "Scissor"
+let str3 = scissor.toLocaleLowerCase("en-US");
 
-let result = game(userChoice, getComputerChoice());
+let result = playRound(userChoice, getComputerChoice());
+
+// let result = game();
 console.log(result);
 
-function game(userSelection, computerSelection) {
-  if (userSelection === computerSelection) {
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
     return "Draw";
   } else if (
-    //logic for winner
-    (userSelection === "Rock" && computerSelection === "Scissor") ||
-    (userSelection === "Paper" && computerSelection === "Rock") ||
-    (userSelection === "Scissor" && computerSelection === "Paper")
+    (playerSelection === str1 && computerSelection === str3) ||
+    (playerSelection === str2 && computerSelection === str1) ||
+    (playerSelection === str3 && computerSelection === str2)
   ) {
     return "Winner";
   } else if (
-    //logic for losers
-    (userSelection === "Rock" && computerSelection === "Paper") ||
-    (userSelection === "Paper" && computerSelection === "Scissor") ||
-    (userSelection === "Scissor" && computerSelection === "Rock")
+    (playerSelection === str1 && computerSelection === str2) ||
+    (playerSelection === str2 && computerSelection === str3) ||
+    (playerSelection === str3 && computerSelection === str1)
   ) {
     return "Loser";
   } else {
-    //for any invalid string submitted by user
     return "Not a valid input";
   }
 }
@@ -31,5 +37,11 @@ function getComputerChoice() {
   const PCchoice = ["Rock", "Paper", "Scissor"];
   const random = Math.floor(Math.random() * PCchoice.length);
   console.log(PCchoice[random]);
-  return PCchoice[random];
+  return PCchoice[random].toLocaleLowerCase("en-US");
 } //takes random number from Computer
+
+// function game() {
+//   for (let i = 0; i < 5; i++)
+
+//   playRound();
+// }
