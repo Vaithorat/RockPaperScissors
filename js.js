543d9065f56c70c50b2bad75117b1f4362dad467
vaithorat <vaibhavthorat141997@gmail.com>
2022-07-31 16:34:18 +0530

    v1.2 Bugs fixed, code running as required

--- js.js
+++ js.js
@@ -1,7 +1,5 @@
-
-
 let userChoice = prompt("Choose Rock, Paper or Scissor");
-console.log(userChoice)
+console.log(userChoice);
 
 let result = game(userChoice, getComputerChoice());
 console.log(result);
@@ -9,26 +7,29 @@ console.log(result);
 function game(userSelection, computerSelection) {
   if (userSelection === computerSelection) {
     return "Draw";
-  } else if ( //logic for winner
+  } else if (
+    //logic for winner
     (userSelection === "Rock" && computerSelection === "Scissor") ||
     (userSelection === "Paper" && computerSelection === "Rock") ||
     (userSelection === "Scissor" && computerSelection === "Paper")
   ) {
     return "Winner";
-  } else if ( //logic for losers
+  } else if (
+    //logic for losers
     (userSelection === "Rock" && computerSelection === "Paper") ||
     (userSelection === "Paper" && computerSelection === "Scissor") ||
     (userSelection === "Scissor" && computerSelection === "Rock")
   ) {
     return "Loser";
-  } else { //for any invalid string submitted by user
+  } else {
+    //for any invalid string submitted by user
     return "Not a valid input";
   }
 }
 
-
 function getComputerChoice() {
   const PCchoice = ["Rock", "Paper", "Scissor"];
   const random = Math.floor(Math.random() * PCchoice.length);
+  console.log(PCchoice[random]);
   return PCchoice[random];
 } //takes random number from Computer
